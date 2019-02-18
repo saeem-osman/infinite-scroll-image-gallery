d25bad17546df73e67643cf05d057d82fa5c85d8
Saeem Osman <saeem.osman@gmail.com>
2019-02-19 02:50:47 +0600

    files added

---
+++ config/server.js
@@ -0,0 +1,8 @@
+module.exports = {
+    APPLICATION_ID: process.env.APPLICATION_ID || 
+        '06022985e953a60ff264e727af8376d45297dcdb548f49c85b6ce389376e983c',
+    SECRET: process.env.SECRET || 
+        '86df29940670b257c24a2e754f6a35f2982481a3428de4d695faccfc3857947d',
+    CALLBACK_URL : process.env.CALLBACK_URL || 'http://localhost:3000'
+
+}
